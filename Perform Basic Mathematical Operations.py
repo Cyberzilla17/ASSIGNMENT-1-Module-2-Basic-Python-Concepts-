a6e8cd862983507da96986a7017c6a517663178b
Cyberzilla17 <112977104+Cyberzilla17@users.noreply.github.com>
2025-04-15 14:21:55 +0530

    Add files via upload

---
+++ Perform Basic Mathematical Operations.py
@@ -0,0 +1,16 @@
+# Take input from user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Perform operations
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2  # No check for zero division
+
+# Display results
+print("\nResults:")
+print("Addition:", add)
+print("Subtraction:", sub)
+print("Multiplication:", mul)
+print("Division:", div)
